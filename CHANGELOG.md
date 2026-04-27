199c77b12228922b12226eb82abfb02ba2288665
Anees Iqbal <hello@aneesiqbal.ai>
2026-04-27 15:56:32 -0700

    :new: Add macCatalyst to supported platforms

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## Upcoming
+
+- Add macCatalyst to supported platforms.
+
+## v1.0.0
+
+- Initial release.
--- Package.swift
+++ Package.swift
@@ -5,11 +5,12 @@ import PackageDescription
 let package = Package(
     name: "JSONCKit",
     platforms: [
-        .macOS(.v10_15),
         .iOS(.v13),
+        .macCatalyst(.v13),
+        .macOS(.v10_15),
         .tvOS(.v13),
-        .watchOS(.v6),
         .visionOS(.v1),
+        .watchOS(.v6),
     ],
     products: [
         .library(
